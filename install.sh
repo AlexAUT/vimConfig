823b6d7bc012ed01bedeeb167e8fdefdef0f45f0
Alexander Weinrauch <alexander.weinrauch@gmail.com>
2018-08-26 14:38:31 +0200

    Updated install script to install vundle automatically

--- install.sh
+++ install.sh
@@ -6,3 +6,21 @@ ln -s $(pwd)/vimrc ~/.vimrc
 
 # Make undo folder, or otherwise vim cannot store the history
 mkdir -p ~/.vim/undo
+
+echo "Insalling vundle..."
+mkdir -o ~/.vim/bundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+
+echo "Installing vim plugins, this may take some while..."
+vim +PluginInstall +qall
+
+echo "Compiling YouCompleteMe, required installed packages:"
+echo "Clang with clang-format"
+echo "Node with npm"
+
+sudo npm install -g typescript
+cd ~/.vim/bundle/YouCompleteMe
+./install.py --clang-completer --js-completer --system-libclang
+
+echo "Finished!"
